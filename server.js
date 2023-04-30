64ac126184f1d408448d25d8d46875a2d48a70e0
Aboagye Gyamfi <gyesboa@gmail.com>
2023-04-30 09:41:28 +0000

    Cors1

--- server.js
+++ server.js
@@ -19,7 +19,14 @@ const app = express();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://riskguardserv.cyclic.app/api",
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+//app.use(cors());
 
 //DB Config
 const db = require("./config/keys").mongoURI;
